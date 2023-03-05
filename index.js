b6f9b163b8fa36ae3896e454cd6c5d6e9a39ca5e
Ezequiel Lessa de oliveira <102971778+ezequiellessa@users.noreply.github.com>
2023-03-05 14:31:07 -0300

    segundo commit

--- index.js
+++ index.js
@@ -21,4 +21,37 @@ buttonTarefa.onclick = function() {
       novoItem.remove();
   };
 
+  // Adiciona o botão Concluído
+  const botaoConcluido = document.createElement('button');
+  botaoConcluido.textContent = 'Concluído';
+  novoItem.appendChild(botaoConcluido);
+  botaoConcluido.classList.add('botaoConcluido');
+  botaoConcluido.classList.add('botaoApagar');
+
+
+ // Adiciona o evento onclick ao botão Concluído
+ botaoConcluido.onclick = function() {
+  novoItem.classList.toggle('liConcluida');
+};
 }
+
+// //  classList.add é um método que adiciona uma ou mais classes a
+//  um elemento HTML, enquanto classList.toggle é um método que adiciona uma classe a um elemento se ela não estiver presente e a remove se já estiver presente.
+
+// // Portanto, a principal diferença entre classList.add e
+//  classList.toggle é que classList.add sempre adiciona uma classe,
+//   enquanto classList.toggle pode adicionar ou remover uma classe, 
+//   dependendo de sua presença atual no elemento.
+
+// // Por exemplo, se um elemento HTML tiver a classe "classe-1"
+// , a chamada de classList.add("classe-1") não terá nenhum efeito, 
+// pois a classe já está presente. Já a chamada de classList.toggle("classe-1") removeria a classe do elemento, pois ela já está presente.
+
+// // Em resumo, use classList.add se você quiser adicionar uma classe
+// independentemente de sua presença atual no elemento, e use 
+// classList.toggle se você quiser alternar a presença de uma
+//  classe no elemento.
+
+
+
+
